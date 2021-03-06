00464efe2716bc8dd1cbb58bbd59b9f0a1c28a3e
Juan Miguel Rodríguez Lago <78445369+juanmiRL@users.noreply.github.com>
2021-03-06 18:41:24 +0100

    create several plots and tab panels

--- app.R
+++ app.R
@@ -8,41 +8,69 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(Stat2Data)
+library(skimr)
+
+data(HorsePrices)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
+ui <- navbarPage("Shiny app Horse Prices",
+                 tabPanel("Data Description",
+                   fluidPage(
+                   mainPanel(
+                     tabsetPanel(type = "tabs",
+                                 tabPanel("Data", tableOutput("Horsedata")),
+                                 tabPanel("Summary", verbatimTextOutput("Horsesummary")),
+                                 tabPanel("Table", tableOutput("Horsetable"))
+                     )
+                 ))),
+                 tabPanel("Univariate Plots",
+                          fluidPage(
+                            sidebarLayout(sidebarPanel(
+                              selectInput("select", label = h3("Choose variable"), 
+                                          choices = list(Price= "Price",Age="Age",Height="Height",Sex="Sex"),
+                                          selected = 1)
+                            ), mainPanel(
+                              h3("Bar plot"),
+                              plotOutput(outputId = "bar")
+                            )
+                            ))),
+                 tabPanel("Bivariate Plots",
+                          fluidPage(
+                            sidebarLayout(position = "right",
+                              sidebarPanel(
+                                 selectInput("select2", label = h3("Choose variable"), 
+                                          choices = list(Age="Age",Height="Height",HorseID="HorseID"),
+                                          selected = 1)
+                            ), mainPanel(
+                              h3("Scatter plot Price by Sex"),
+                              plotOutput(outputId = "scatter")
+                            )
+                            )))
+                 #  titlePanel
+) # navbarPage
+
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+  output$Horsedata <- renderTable(skim(HorsePrices))
+  
+  output$Horsesummary <- renderPrint(summary(HorsePrices))
+  
+  output$Horsetable <- renderTable(HorsePrices)
+  
+  output$bar <- renderPlot({
+    ggplot(HorsePrices)+ xlab("")+
+      geom_boxplot(aes(input$select))+ theme_minimal()
+  })
+  
+  output$scatter <- renderPlot({
+    ggplot(HorsePrices,aes(x= input$select2,y=Price,colour=Sex))+ xlab("")+
+      geom_jitter()+ theme_minimal()
+  })
 }
 
 # Run the application 
