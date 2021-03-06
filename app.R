e5ab9fc24851b0c2ccc7290fa4eda26549ab2fa0
Juan Miguel Rodríguez Lago <78445369+juanmiRL@users.noreply.github.com>
2021-03-06 21:40:15 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -8,43 +8,132 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(Stat2Data)
+library(skimr)
+
+data(HorsePrices)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
+ui <- navbarPage("Shiny app Horse Prices",
+                 tabPanel("Data Description",
+                   fluidPage(
+                   mainPanel(
+                     tabsetPanel(type = "tabs",
+                                 tabPanel("Table", tableOutput("Horsetable")),
+                                 tabPanel("Description", tableOutput("Horsedata")),
+                                 tabPanel("Summary", verbatimTextOutput("Horsesummary"))
+                                 
+                     )
+                 ))),
+                 tabPanel("Univariate Plots",
+                          fluidPage(
+                            mainPanel(tabsetPanel(type = "tabs",
+                                                  tabPanel("Price", plotOutput("price")),
+                                                  tabPanel("Age", plotOutput("age")),
+                                                  tabPanel("Height", plotOutput("height")),
+                                                  tabPanel("Sex", plotOutput("sex"))
+                            )
+                            
+                            ))),
+                            
+                 tabPanel("Bivariate Plots",
+                          fluidPage(
+                            sidebarLayout(position = "right",
+                              sidebarPanel(
+                                 selectInput("select2", label = h3("Choose variable"), 
+                                          choices = list(Age="Age",Height="Height",HorseID="HorseID"),
+                                          selected = 1)
+                            ), mainPanel(
+                              h3("Scatter plot Price by Sex"),
+                              plotOutput(outputId = "scatter")
+                            )
+                            ))),
+                 tabPanel("Regression model",
+                          fluidPage(
+                            mainPanel(
+                              tabsetPanel(type = "tabs",
+                                          tabPanel("Multiple Regression Summary", verbatimTextOutput("Modelsummary")),
+                                          tabPanel("Price ~ Age", plotOutput("Modelplot1")),
+                                          tabPanel("Price ~ Height", plotOutput("Modelplot2")),
+                                          tabPanel("Price by Sex", plotOutput("Modelplot3"))
+                              )
+                            ))),
+                 tabPanel("Predict new horse price",
+                          fluidPage(
+                            sidebarLayout(sidebarPanel(
+                            sliderInput("Age", label = h3("Age of horse"), min = 0, 
+                                        max = 25, value = 10),
+                            sliderInput("Height", label = h3("Height of horse"), min = 14, 
+                                        max = 20, value = 16),
+                            selectInput("Sex", label = h3("Sex"),choices =c(Female="f",Male="m"))
+                            ),
+                            
+                            mainPanel(
+                              h3("The price of the new horse is:"),
+                              textOutput("predict")
+                            )
+                          )
+                 ))
+                 
+                 #  titlePanel
+) # navbarPage
+
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+  output$Horsedata <- renderTable(skim(HorsePrices))
+  
+  output$Horsesummary <- renderPrint(summary(HorsePrices))
+  
+  output$Horsetable <- renderTable(HorsePrices)
+  
+  output$price <- renderPlot({
+    ggplot(HorsePrices)+ 
+      geom_boxplot(aes(Price),fill="tomato2")+ coord_flip()+ theme_minimal()+xlab("Price")
+  })
+  output$height <- renderPlot({
+    ggplot(HorsePrices)+
+      geom_boxplot(aes(Height),fill="gold3")+ coord_flip()+ theme_minimal()+xlab("Height")
+  })
+  output$sex <- renderPlot({
+    ggplot(HorsePrices, aes(fill=Sex))+ xlab("Sex")+
+      geom_bar(aes(Sex))+ theme_minimal()
+  })
+  output$age <- renderPlot({
+    ggplot(HorsePrices)+ 
+      geom_density(aes(Age),fill="orange",alpha=0.2)+ theme_minimal()+xlab("Age")
+  })
+  
+  output$scatter <- renderPlot({
+    ggplot(HorsePrices,aes(x= input$select2,y=Price,colour=Sex))+ xlab("")+
+      geom_jitter()+ theme_minimal()
+  })
+  
+  output$Modelsummary <- renderPrint(summary(lm(Price~Age+Height+Sex,HorsePrices)))
+  
+  
+  output$Modelplot1 <- renderPlot({
+    ggplot(HorsePrices,aes(x= Age,y=Price))+ 
+      geom_jitter()+ geom_smooth()+ theme_minimal()
+  })
+  
+  output$Modelplot2 <- renderPlot({
+    ggplot(HorsePrices,aes(x= Height,y=Price))+ 
+      geom_jitter()+ geom_smooth()+ theme_minimal()
+  })
+  
+  output$Modelplot3 <- renderPlot({
+    ggplot(HorsePrices,aes(x= Sex,y=Price,col=Sex))+ 
+      geom_boxplot()+ theme_minimal()
+  })
+  
+  output$predict <- renderText(predict(lm(Price~Age+Height+Sex,HorsePrices),data.frame(Age=input$Age, Height=input$Height, Sex=input$Sex)))
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
 
